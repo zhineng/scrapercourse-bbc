255dc3ab1fa6b5bd48fc254a32a509f7318aa067
zhineng <zhineng@gmail.com>
2022-05-11 00:31:52 +0800

    3.5批次爬取多分頁的網頁資料

--- scraper.py
+++ scraper.py
@@ -2,35 +2,29 @@ from gettext import gettext
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get(
-    "https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt")
+for page in range(1, 4):
+    response = requests.get(
+        f'https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt/page/{page}')
 
-# print(response.text.find('亞馬遜將為美國員工報銷墮胎和其它治療的差旅費'))
+    soup = BeautifulSoup(response.text, 'lxml')
 
-soup = BeautifulSoup(response.text, 'lxml')
-# tilte = soup.find(
-#    'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
-# print(tilte.getText())
+    titles = soup.find_all(
+        'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
 
-tiltes = soup.find_all(
-    'span', {'class': 'lx-stream-post__header-text gs-u-align-middle'})
-# print(tiltes)
+    title_list = []
+    for title in titles:
+        title_list.append(title.getText())
 
-title_list = []
-for title in tiltes:
-    title_list.append(title.getText())
-# print(title_list)
+    urls = soup.find_all(
+        'a', {'class': 'qa-heading-link lx-stream-post__header-link'})
+    tag_list = []
+    for url in urls:
+        sub_response = requests.get('https://www.bbc.com' + url.get('href'))
+        sub_soup = BeautifulSoup(sub_response.text, 'lxml')
+        tags = sub_soup.find_all('li', {'class': 'bbc-1msyfg1 e1hq59l0'})
+        for tag in tags:
+            tag_list.append(tag.getText())
 
-
-urls = soup.find_all(
-    'a', {'class': 'qa-heading-link lx-stream-post__header-link'})
-tags_list = []
-for url in urls:
-    #print('https://www.bbc.com' + url.get('href'))
-    sub_response = requests.get('https://www.bbc.com' + url.get('href'))
-    sub_soup = BeautifulSoup(sub_response.text, 'lxml')
-    tags = sub_soup.find_all('li', {'class': 'bbc-1msyfg1 e1hq59l0'})
-    for tag in tags:
-        tags_list.append(tag.find('a').getText())
-
-print(tags_list)
+    print(f'第{page}頁')
+    print(title_list)
+    print(tag_list)
